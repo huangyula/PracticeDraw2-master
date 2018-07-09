e1607cadac023573f43481f974a58ea321137e95
huangyu <13924451842@163.com>
2018-07-09 14:03:54 +0800

    作业完成

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice03SweepGradientView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice03SweepGradientView.java
@@ -2,13 +2,17 @@ package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Shader;
+import android.graphics.SweepGradient;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice03SweepGradientView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Shader mShader;
 
     public Practice03SweepGradientView(Context context) {
         super(context);
@@ -25,6 +29,8 @@ public class Practice03SweepGradientView extends View {
     {
         // 用 Paint.setShader(shader) 设置一个 SweepGradient
         // SweepGradient 的参数：圆心坐标：(300, 300)；颜色：#E91E63 到 #2196F3
+        mShader=new SweepGradient(300,300, Color.parseColor("#E91E63"),Color.parseColor("#2196F3"));
+        paint.setShader(mShader);
     }
 
     @Override
